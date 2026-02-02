274c275059d4da9b18e62d12a6e542f331c55d18
Sepp Vallant <jo.va@gmx.at>
2026-02-02 19:32:54 +0100

    new Version V2.6.6

--- www/builder/plugins/gridstack/index-jq.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export * from './utils';
-export * from './gridstack-engine';
-export * from './gridstack-ddi';
-export * from './gridstack';
-export * from './jq/gridstack-dd-jqueryui';
--- www/builder/plugins/gridstack/types.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// types.ts 3.1.0 @preserve
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
